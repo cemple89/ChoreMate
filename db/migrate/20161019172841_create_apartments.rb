e5937880f943c5c45ac869ff2ce9f398ba313c98
Charlie Emple <charlie.emple@gmail.com>
2016-10-19 13:52:43 -0400

    initial migrations

--- db/migrate/20161019172841_create_apartments.rb
+++ db/migrate/20161019172841_create_apartments.rb
@@ -1,6 +1,10 @@
 class CreateApartments < ActiveRecord::Migration[5.0]
   def change
     create_table :apartments do |t|
+      t.string :address, null: false
+      t.string :city, null: false
+      t.string :state, null: false
+      t.string :zip_code, null: false 
     end
   end
 end
