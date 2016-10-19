55b623119fe76dda5c52cd66e42253cc5304cd69
Charlie Emple <charlie.emple@gmail.com>
2016-10-19 14:38:23 -0400

    model validations

--- app/models/apartment.rb
+++ app/models/apartment.rb
@@ -1,5 +1,11 @@
 class Apartment < ActiveRecord::Base
   has_many :users
-  has_many :chores 
+  has_many :chores
+
+  validates :address, presence: true
+  validates :city, presence: true
+  validates :state, presence: true
+  validates :zip_code, presence: true
+  validates :zip_code, numericality: { only_integer: true }, length: { is: 5 }
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ActiveRecord::Base
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :apartment_id, presence: true
+  
   has_many :chores
   belongs_to :apartment
 
