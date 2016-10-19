ecc5f9b9f22fe242999a346cf5022ad3696a95d8
Charlie Emple <charlie.emple@gmail.com>
2016-10-19 19:22:52 -0400

    user can add chore test

--- spec/user_can_add_chore.rb
+++ spec/user_can_add_chore.rb
@@ -1,3 +1,30 @@
 require 'rails-helper'
 
-describe 'User can'
+describe 'User can' do
+
+  let (:user_1) { FactoryGirl.create(:user)}
+  let (:apartment_1) { FactoryGirl.create(:apartment)}
+
+
+  feature 'add a location' do
+
+    scenario 'user adds a new chore successfully'
+      visit new_chore_path
+      fill_in 'Name', with: 'Swiffer the Kitchen'
+      fill_in 'Days Between Completion', with: '7'
+
+      click_button 'Add Chore'
+
+      expect_page.to have_content 'Chore added successfully'
+      expect_page.to have_content 'Swiffer the Kitchen'
+    end
+
+    scenario 'user does not provide proper information for a chore'
+      visit new_chore_path
+      click_button 'Add Chore'
+      expect(page).to have_content 'Name can\'t be blank'
+      expect(page).to have_content 'Days Between Completion can\'t be blank'
+      expect(page).to have_content 'Days Between Completion is not a number'
+    end
+  end
+end 
