2a4ce11200237d0459c140d7941cd4bb794b68d1
Charlie Emple <charlie.emple@gmail.com>
2016-10-20 11:07:12 -0400

    chores controller

--- app/controllers/chores_controller.rb
+++ app/controllers/chores_controller.rb
@@ -1,7 +1,27 @@
 class ChoresController < ActionController::Base
 
+  def new
+    @chores = Chore.new
+  end
+
+  def create
+    @apartment = current_user.apartment
+    @chore = Chore.new(chore_params, apartment: @apartment)
+    if @chore.save
+  end
+
+  def index
+    @apartment = current_user.apartment
+    @chores = Chore.find_by(apartment: @apartment)
+    @user = @apartment.users 
+  end
+
 
   protected
+
+  def chore_params
+    params.require(:chore).permit(:name, :completion_interval, :apartment_id)
+  end
 
 
 end
