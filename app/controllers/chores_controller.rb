e26058b0c0c0c1ea3c90e35a29d60dca673bacee
Charlie Emple <charlie.emple@gmail.com>
2016-10-20 18:35:36 -0400

    changed users to be hard coded and fixed forms

--- app/controllers/chores_controller.rb
+++ app/controllers/chores_controller.rb
@@ -1,30 +1,37 @@
 class ChoresController < ActionController::Base
 
   def new
-    # @apartment = current_user.apartment
-    @chore = Chore.new(chore_params, apartment: @apartment)
+    @user = User.find(1)
+    @apartment = @user.apartment
+    @chore = Chore.new
     @points_collection = Chore::POINTS
   end
 
   def create
-    @apartment = current_user.apartment
-    @chore = Chore.new(chore_params, apartment: @apartment)
+    @points_collection = Chore::POINTS
+    @user = User.find(1)
+    @apartment = @user.apartment
+    @chore = Chore.new(chore_params)
+    @chore.apartment = @apartment
     if @chore.save
       flash[:notice] = 'Chore added successfully'
-      redirect_to apartment_path
+      redirect_to root_path
     else
       flash[:notice] = @chore.errors.full_messages.join(", ")
-      redirect_to apartment_path
+      render :new
+    end
   end
 
   def index
-    @apartment = current_user.apartment
+    @user = User.find(1)
+    @apartment = @user.apartment
     @chores = Chore.find_by(apartment: @apartment)
     @users = @apartment.users
   end
 
   def edit
-    @apartment = current_user.apartment
+    @user = User.find(1)
+    @apartment = @user.apartment
     @chore = Chore.new(chore_params, apartment: @apartment)
     @points_collection = Chore::POINTS
   end
@@ -33,7 +40,7 @@ class ChoresController < ActionController::Base
   protected
 
   def chore_params
-    params.require(:chore).permit(:name, :completion_interval, :apartment_id)
+    params.require(:chore).permit(:name, :completion_interval, :apartment_id, :points)
   end
 
 
--- app/models/chore.rb
+++ app/models/chore.rb
@@ -7,12 +7,12 @@ class Chore < ActiveRecord::Base
            ]
 
   belongs_to :apartment
-  belongs_to :user
+  has_many :users
 
   validates :name, presence: true
   validates :points, presence: true
-  validates :points, numericality: { only_integer: true }
-  validates :points, inclusion: 1...3
+  # validates :points, numericality: { only_integer: true }
+  # validates :points, inclusion: 1...3
   validates :completion_interval, numericality: { only_integer: true }
   validates :completion_interval, presence: true
   validates :apartment_id, presence: true
