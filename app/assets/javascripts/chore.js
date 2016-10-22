3692cb919d16e531ab5dde2abd64d2e3fd57a0ad
Charlie Emple <charlie.emple@gmail.com>
2016-10-22 16:36:23 -0400

    chorescore update

--- app/assets/javascripts/chore.js
+++ app/assets/javascripts/chore.js
@@ -5,24 +5,28 @@ $(document).ready(function() {
     var self = $(this);
     user = self.data('user');
     chore = self.data('chore');
-    points = self.data('points')
+    points = self.data('points');
+    score = self.data('score');
+    var user_chorescore = $("#user-chorescore-" + user);
+
     var claim_chore = $.ajax({
        type: "GET",
        data: {user_id: user},
        url: `/chores/${chore}/edit`
+
   })
   claim_chore.done(data => {
     document.getElementById('chore-claim-message').innerHTML = data.message;
     })
-
     var chore_score = $.ajax({
-      type: "GET",
-      data: {chorescore: points}
-      url: `/users/${user}/edit`
+      type: "PATCH",
+      data: {chore_points: points},
+      url: `/users/${user}`
+
     })
-    chore_score.done(data) => {
-      document.getElementById('chore-score-message').innerHTML =
-      data.message;
+
+    chore_score.done(data => {
+      document.getElementById(user_chorescore).innerHTML = (score + points)
     });
   });
 })
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,13 @@
 class UsersController < ApplicationController
 
 
+  def update
+    @user = User.find(1)
+    @user.chorescore += params[:chore_points].to_i
+    @user.save
+  end
+
+
   protected
 
 
