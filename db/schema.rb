f9959515eb9b9947469b13fee563a6cf8c094319
Charlie Emple <charlie.emple@gmail.com>
2016-10-27 19:21:28 -0400

    added admin column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161024173734) do
+ActiveRecord::Schema.define(version: 20161027232040) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 20161024173734) do
     t.string   "uid"
     t.string   "oauth_token"
     t.datetime "oauth_expires_at"
+    t.boolean  "admin"
     t.index ["apartment_id"], name: "index_users_on_apartment_id", using: :btree
   end
 
